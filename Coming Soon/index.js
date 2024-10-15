13bce6d7873acc191bc39162a2a9cf8d15fb3a1c
oluwabillionz96 <goodluckreuben96@gmail.com>
2024-10-15 07:37:47 -0700

    project completed

--- Coming Soon/index.js
+++ Coming Soon/index.js
@@ -1,10 +1,24 @@
-const image = document.querySelector('#images img');
-let number;
+// Getting Elements
+const button = document.getElementById('send-icon');
+const error = document.querySelector('.error-i');
+const errorMessage = document.getElementById('error');
+const inputBorder = document.querySelector('.input-wrapper');
+const input = document.querySelector('.maill'); 
+console.log(input);
 
-if(document.body.clientWidth < 480){
-    number = 2;
-    image.setAttribute('src','./assets/hero'+ number +'.jpg')
-} else if(document.body.clientWidth > ){
-    number = 1;
-    image.setAttribute('src','./assets/hero'+ number +'.jpg')
-}
+// Adding Event listeners
+
+button.addEventListener('click', function(){
+
+    if(input.value.includes('@') === true && input.value.includes('.com') === true){
+       inputBorder.style.borderColor = 'green';
+       input.value = '';
+       errorMessage.style.display = 'none';
+       error.style.display = 'none';
+    } else if(input.value === ''|| input.value.includes('@') === false || input.value.includes('.com') === false){
+        errorMessage.style.display = 'block';
+        inputBorder.style.borderColor = 'red';
+        error.style.display = 'inline';
+    }
+    }) 
+
